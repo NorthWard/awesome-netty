8c8e59c6a87babdc0e76cd3b5d64e78cec08d3d4
laihaohua <lhh.cs@qq.com>
2019-11-17 19:59:41 +0800

    增加注释

--- awesome-netty-redis/src/main/java/com/north/netty/redis/clients/AbstractRedisClient.java
+++ awesome-netty-redis/src/main/java/com/north/netty/redis/clients/AbstractRedisClient.java
@@ -23,15 +23,20 @@ public abstract class AbstractRedisClient<T> implements RedisClient<T> {
         RedisConnection<T> connection = null;
         try{
             T data = cmd.build();
+            // 从连接池中borrow连接
             connection = connectionPool.borrowConnection();
             if(connectionPool.checkChannel(connection)){
+                // 要锁定这个连接
                 connection.lock();
                 try{
+                    // 发送命令
                     connection.writeAndFlush(data).sync();
+                    // 获取命令的返回结果
                     return cmdResp.parseResp(connection.getResp(RedisConfig.TIME_OUT_MS));
                 }catch (Exception e){
                    e.printStackTrace();
                 }finally {
+                    // 释放锁
                     connection.unlock();
                 }
             }else{
@@ -41,6 +46,7 @@ public abstract class AbstractRedisClient<T> implements RedisClient<T> {
             e.printStackTrace();
         }finally {
             if(connectionPool.checkChannel(connection)){
+                // 把连接放回到连接池
                 connectionPool.returnConnection(connection);
             }
         }
